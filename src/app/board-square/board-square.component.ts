ba5a3a0446ccf27a7e8f58263e8d7db125d97357
Matthew Nichols <matthew@matthew-nichols.com>
2017-06-29 20:54:13 -0600

    Made the classing of app-board-square state driven

--- src/app/board-square/board-square.component.ts
+++ src/app/board-square/board-square.component.ts
@@ -1,15 +1,25 @@
-import { Component, Input } from '@angular/core';
+import {Component, HostBinding, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-board-square',
   templateUrl: './board-square.component.html',
   styleUrls: ['./board-square.component.scss']
 })
-export class BoardSquareComponent {
+export class BoardSquareComponent implements OnInit{
 
   constructor() { }
 
   @Input() row: number;
   @Input() col: number;
+
+  @HostBinding('class.center-column') centerColumn: boolean;
+  @HostBinding('class.center-row') centerRow: boolean;
+
+  ngOnInit(): void {
+    console.log(this.col);
+    this.centerColumn = this.col == 2;
+    this.centerRow = this.row == 2;
+  }
+
 
 }
