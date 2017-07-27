fd1ad286cbfde61e0b474a5173ec59e88bfbd0a6
Matthew Nichols <matthew@matthew-nichols.com>
2017-07-26 18:47:12 -0600

    Converted the other getActions to async

--- src/app/services/code-management.service.ts
+++ src/app/services/code-management.service.ts
@@ -7,8 +7,8 @@ const WORKING_SCRIPT_STORAGE_KEY = "WORKING_SCRIPT_STORAGE_KEY";
 const SCRIPT_LIST_STORAGE_KEY = "SCRIPT_LIST_STORAGE_KEY";
 
 export interface ICodeManagementService {
-  getSavedScriptsList(): scriptDescriptor[];
-  getSavedScript(scriptId: string): savedScript;
+  getSavedScriptsList(): Promise<scriptDescriptor[]>;
+  getSavedScript(scriptId: string): Promise<savedScript>;
 
   /**
    * Deletes the specified savedScript object and
@@ -22,7 +22,7 @@ export interface ICodeManagementService {
    * in the SavedScripts list.
    * @param script
    */
-  saveScript(script: savedScript);
+  saveScript(script: savedScript): void;
 
   /**
    * Get the current working script
@@ -43,22 +43,20 @@ export class CodeManagementService implements ICodeManagementService {
       const defaultValue = new savedScript({});
       const key = WORKING_SCRIPT_STORAGE_KEY + playerId;
       let script = localStorageUtils.getItemAs(savedScript, key, defaultValue);
-      console.log(script.scriptText);
       resolve(script);
     });
     //return localStorageUtils.getItemAs(savedScript, WORKING_SCRIPT_STORAGE_KEY + playerId, new savedScript({}));
   }
 
   saveWorkingScript(playerId: number, script: savedScript) {
-    console.log("saving", playerId, savedScript);
     localStorageUtils.saveItem(WORKING_SCRIPT_STORAGE_KEY + playerId, script);
   }
 
-  getSavedScriptsList(): scriptDescriptor[] {
+  getSavedScriptsList(): Promise<scriptDescriptor[]> {
     throw new Error("Method not implemented.");
   }
 
-  getSavedScript(scriptId: string): savedScript {
+  getSavedScript(scriptId: string): Promise<savedScript> {
     throw new Error("Method not implemented.");
   }
 
@@ -66,7 +64,7 @@ export class CodeManagementService implements ICodeManagementService {
     throw new Error("Method not implemented.");
   }
 
-  saveScript(script: savedScript) {
+  saveScript(script: savedScript): void {
     throw new Error("Method not implemented.");
   }
 
